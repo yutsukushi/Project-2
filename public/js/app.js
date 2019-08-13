fb64de7b5dcfec7807109960c4069603ac4c69bf
yutsukushi <ykushibi@gmail.com>
2019-08-13 16:47:07 -0400

    Set up api connection(still not connected), created a recentsearch model, recentsearch mySQL table, .post method for taking user input and saving to the database, began set up on .get method for getting db data to handlebars table

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,21 @@
-// inputs and button functions
-// module.exports = function () {
-//   $("#submit").on("click", function (event) {
-//     event.preventDefault;
-//     valid = true;
+$("#apiSubmit").on("click", function() {
+  var apiUserInput = $(".api")
+    .val()
+    .trim();
+  console.log(apiUserInput);
+  var queryURL =
+    "https://api.co2signal.com/v1/latest?countryCode=" + apiUserInput;
 
-//     //form validation function
-//     if ($(".country").val() !== ""){
-//       valid = true;
-//     //   alert("Please enter search criteria.");
-//     // } else if(true) {
-//       //input values of the country
-//       var country = $(".country").val().trim();
+  var apiKey = "83c13d239527c902";
 
-//       $.ajax("/footprintstats/" + country,
-//         {
-//           type: "GET"
-//         }).then(function (results) {
-
-//           console.log("its working", results);
-//         })
-//       ;
-//     }
-//     //get request from the api and the data set
-//   });
-// // };
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    dataType: "jsonp",
+    headers: {
+      "auth-token": apiKey
+    }
+  }).then(function(response) {
+    console.log(response);
+  });
+});
